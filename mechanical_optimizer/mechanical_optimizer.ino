ec49b63b35c13c8e0724a8414da180a024d707ff
Michael Lu <mlu0708@mit.edu>
2021-05-13 02:59:37 -0400

    Config for geared 360 degree servo

--- mechanical_optimizer/mechanical_optimizer.ino
+++ mechanical_optimizer/mechanical_optimizer.ino
@@ -1,4 +1,4 @@
-#include <Servo.h>
+#include <VarSpeedServo.h>
 #include <LiquidCrystal.h>
 #include <string.h>
 #include <stdlib.h>
@@ -10,7 +10,7 @@ const uint8_t lcd_data_precision = 5;
 const uint8_t data_len = 64;
 
 const uint8_t min_sweep_angle = 0;
-const uint8_t max_sweep_angle = 180;
+const uint8_t max_sweep_angle = 150;
 uint8_t low = min_sweep_angle;
 uint8_t high = max_sweep_angle;
 int decay_rate = 3;
@@ -22,35 +22,66 @@ int f, s11, s21;
 char data[data_len];
 boolean new_data = false;
 
-Servo servo;
+VarSpeedServo servo;
 const uint8_t servo_pin = 9;
 
 void setup() {
   Serial.begin(115200);
 
   servo.attach(servo_pin);
-  servo.write(0);
   
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
   lcd.print("Init...");
-  
-  delay(3000);
+
+  servo.write(min_sweep_angle, 20);
+  delay(4000);
+  servo.write(max_sweep_angle, 20);
+  delay(4000);
+  servo.write(min_sweep_angle, 20);
+  delay(4000);
+  servo.write(max_sweep_angle, 20);
+  delay(4000);
 }
 
 void loop() {
-  read_vna_data();
+  while (low < high) {
+    int span = high - low;
+    int angle = low;
+    int max_angle = low;
+    int max_s21 = 0;
+
+    while (angle >= low && angle <= high) {
+      servo.write(angle);
+      read_vna_data();
       
-  if (new_data) {
-    s21 = atoi(data);
-    lcd.setCursor(0, 0);
-    lcd.print("    S21: ");
-    lcd.print(s21);
-    lcd.setCursor(0, 1);
-    lcd.print("Max S21: ");
-    lcd.print(max_s21);
-    
-    new_data = false;
+      if (new_data) {
+        if (s21 > max_s21) {
+          max_s21 = s21;
+          max_angle = angle;
+        }
+        
+        s21 = atoi(data);
+        lcd.setCursor(0, 0);
+        lcd.print("    S21: ");
+        lcd.print(s21);
+        lcd.setCursor(0, 1);
+        lcd.print("Max S21: ");
+        lcd.print(max_s21);
+        
+        new_data = false;
+      
+  
+        angle += sample_step;
+  
+        delay(sample_delay);
+      }
+    }
+
+    sample_step = max(sample_step - 1, 0);
+    span /= decay_rate;
+    low = max(max_angle - span, min_sweep_angle);
+    high = min(max_angle + span, max_sweep_angle);
   }
 }
 
